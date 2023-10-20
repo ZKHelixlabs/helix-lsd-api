4d6f313cf3bbf68ced2c0c9d78e0a8a2e43ac461
HavenCross <havenquan@qq.com>
2023-10-20 15:43:03 +0800

    dev

--- source/controllers/cardano.ts
+++ source/controllers/cardano.ts
@@ -147,8 +147,7 @@ const mint = async (req: Request, res: Response, next: NextFunction) => {
 
     tx = await cli.transaction.sign({ tx, privateKey: paymentPrivateKey });
 
-    // const txid = (await koios.tx.submit(tx)).toString();
-    const txid = "";
+    const txid = (await koios.tx.submit(tx)).toString();
     console.log(txid);
 
     return res.status(200).json({ status: "ok", data: { txid, stADAAmount } });
