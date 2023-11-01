bcfefc4897b85a41c635ed257af077c8bbae0291
HavenCross <havenquan@qq.com>
2023-11-01 18:42:24 +0800

    dev

--- source/controllers/cardano.ts
+++ source/controllers/cardano.ts
@@ -97,7 +97,7 @@ const mint = async (req: Request, res: Response, next: NextFunction) => {
       );
     }
 
-    const beneficiaryWithStakeADAUTxO = (await cli.query.utxo({ address: beneficiaryWithStake })).find((u: UTxO) => u.resolved.value.map.find((item: any) => item.policy.toString() == "" && item.assets[""] >= 1_000_000n));
+    const beneficiaryWithStakeADAUTxO = (await cli.query.utxo({ address: beneficiaryWithStake })).find((u: UTxO) => u.resolved.value.map.find((item: any) => item.policy.toString() == "" && item.assets[""] >= 1_000_000n && item.assets[""] < 1000_000_000n));
 
     console.log('beneficiaryWithStakeADAUTxO: ', beneficiaryWithStakeADAUTxO);
 
