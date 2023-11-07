33a46f07bc13a8561fab9f8e2c7d46a75a9a6fd8
HavenCross <havenquan@qq.com>
2023-11-08 02:30:32 +0800

    dev

--- source/controllers/cardano.ts
+++ source/controllers/cardano.ts
@@ -368,7 +368,7 @@ const withdraw = async (req: Request, res: Response, next: NextFunction) => {
             beneficiary: pBSToData.$(pByteString(beneficiary.paymentCreds.hash.toBuffer())),
             status: pIntToData.$(1),
             oldValue: pIntToData.$(oldValue && oldValue.int > 0 ? oldValue.int : oldAdaAmount),
-            oldTime: pIntToData.$(oldTime && oldTime.int ? oldTime.int.toString() : new Date().getTime()),
+            oldTime: pIntToData.$(oldTime && oldTime.int ? new Date(oldTime.int).getTime() : new Date().getTime()),
           })
         },
       ],
