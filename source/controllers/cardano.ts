338c0a1aaa163ff911e918fe2c31d69fcb8b8b48
HavenCross <havenquan@qq.com>
2024-02-22 17:12:58 +0800

    console

--- source/controllers/cardano.ts
+++ source/controllers/cardano.ts
@@ -40,6 +40,8 @@ const mint = async (req: Request, res: Response, next: NextFunction) => {
           isData(datum)
         ) {
           const pkh = datum.toJson();
+          console.log(pkh);
+          console.log(value);
 
           // search if it corresponds to one of my public keys
           const myPkhIdx = userAddrs.findIndex(
